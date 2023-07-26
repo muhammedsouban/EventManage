3a04ca5c7ba0a07618fea010d73d3a0863984f2d
muhammedsouban <muhammedsoubanbi@gmail.com>
2023-07-26 09:32:40 +0530

    first commit

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar: React.FC<NavbarProps> = () => {
   return (
     <nav className="flex justify-between items-center p-4 bg-gray-800 text-white">
       <div className="flex items-center">
-       <h1 className="font-bold uppercase text-lg">Shedule</h1>
+       <h1 className="font-bold uppercase text-lg">Schedule</h1>
       </div>
       <div className="flex items-center">
       <Link href="/">
